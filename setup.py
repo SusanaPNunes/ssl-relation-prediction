bdabe62e24d3b2b9dedc970bfb23f6abc285e310
Susana Nunes <68297485+SusanaPNunes@users.noreply.github.com>
2024-11-04 11:51:38 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# Dentro do repositório ssl-relation-prediction
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ssl-relation-prediction",
+    version="0.1",
+    packages=find_packages(),
+)
